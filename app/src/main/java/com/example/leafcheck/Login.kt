ce893bd7e1849d7dde27a6875e33651fba945606
Muhammad Hilmi Adzkia <103296966+MHilmiAdz@users.noreply.github.com>
2025-02-02 22:43:39 +0700

    feat: new layout for login
    completed layout for login activity, declare and initialize the buttons, text and view to kotlin logic

--- app/src/main/java/com/example/leafcheck/Login.kt
+++ app/src/main/java/com/example/leafcheck/Login.kt
@@ -1,12 +1,26 @@
 package com.example.leafcheck
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Login : AppCompatActivity() {
+
+    private lateinit var loginText: TextView
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var loginButton: Button
+    private lateinit var forgotpassbutton: TextView
+    private lateinit var signupText: TextView
+    private lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +31,43 @@ class Login : AppCompatActivity() {
             insets
         }
 
+        loginText = findViewById(R.id.loginText)
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        loginButton = findViewById(R.id.loginButton)
+        forgotpassbutton = findViewById(R.id.forgotpassbutton)
+        signupText = findViewById(R.id.signupText)
+        imageView = findViewById(R.id.imageView)
 
+        loginButton.setOnClickListener {
+            // Handle login button click
+            goToMain()
+        }
+//        forgotpassbutton.setOnClickListener {
+//            // Handle forgot password click
+//            goToForgotPassword()
+//        }
+        signupText.setOnClickListener {
+            // Handle signup click
+            goToRegister()
+        }
+    }
+
+    // Function to navigate to the Register activity
+    private fun goToRegister() {
+        val intent = Intent(this, Register::class.java)
+        startActivity(intent)
+    }
+
+    // Function to navigate to the ForgotPassword activity
+//    private fun goToForgotPassword() {
+//        val intent = Intent(this, ForgotPassword::class.java)
+//        startActivity(intent)
+//    }
+
+    // Function to navigate to the Main activity
+    private fun goToMain() {
+        val intent = Intent(this, LeafCheck::class.java)
+        startActivity(intent)
     }
 }
